5133dce0e5b8692ddfeba2aacd19fe9d1638fcf0
Ian Goodacre <Ian.Goodacre@xtra.co.nz>
2024-06-20 22:15:20 +1200

    Update dependencies and reimplement tests
    All dependencies up to date.
    
    Replace mocha with tape.
    
    Use @ig3/eslint-config-entrain.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-'use strict'
+'use strict';
 
-const Plugin = require('markdown-it-regexp')
-const extend = require('extend')
-const sanitize = require('sanitize-filename')
+const Plugin = require('markdown-it-regexp');
+const extend = require('extend');
+const sanitize = require('sanitize-filename');
 
 module.exports = (options) => {
-
   const defaults = {
     linkPattern: /\[\[([-\w\s/]+)(\|([-\w\s/]+))?\]\]/,
     baseURL: '/',
@@ -15,73 +14,71 @@ module.exports = (options) => {
     htmlAttributes: {
     },
     generatePageNameFromLabel: (label) => {
-      return label
+      return label;
     },
     postProcessPageName: (pageName) => {
-      pageName = pageName.trim()
-      pageName = pageName.split('/').map(sanitize).join('/')
-      pageName = pageName.replace(/\s+/g, '_')
-      return pageName
+      pageName = pageName.trim();
+      pageName = pageName.split('/').map(sanitize).join('/');
+      pageName = pageName.replace(/\s+/g, '_');
+      return pageName;
     },
     postProcessLabel: (label) => {
-      label = label.trim()
-      return label
-    }
+      label = label.trim();
+      return label;
+    },
+  };
+
+  options = extend(true, defaults, options);
+
+  function isAbsolute (pageName) {
+    return options.makeAllLinksAbsolute || pageName.charCodeAt(0) === 0x2F; /* / */
   }
 
-  options = extend(true, defaults, options)
-
-  function isAbsolute(pageName) {
-    return options.makeAllLinksAbsolute || pageName.charCodeAt(0) === 0x2F/* / */
-  }
-
-  function removeInitialSlashes(str) {
-    return str.replace(/^\/+/g, '')
+  function removeInitialSlashes (str) {
+    return str.replace(/^\/+/g, '');
   }
 
   return Plugin(
     options.linkPattern,
     (match, utils) => {
-      let label = ''
-      let pageName = ''
-      let href = ''
-      let htmlAttrs = []
-      let htmlAttrsString = ''
-      const isSplit = !!match[3]
+      let label = '';
+      let pageName = '';
+      let href = '';
+      const htmlAttrs = [];
+      let htmlAttrsString = '';
+      const isSplit = !!match[3];
       if (isSplit) {
-        label = match[3]
-        pageName = match[1]
-      }
-      else {
-        label = match[1]
-        pageName = options.generatePageNameFromLabel(label)
+        label = match[3];
+        pageName = match[1];
+      } else {
+        label = match[1];
+        pageName = options.generatePageNameFromLabel(label);
       }
 
-      label = options.postProcessLabel(label)
-      pageName = options.postProcessPageName(pageName)
+      label = options.postProcessLabel(label);
+      pageName = options.postProcessPageName(pageName);
 
       // make sure none of the values are empty
       if (!label || !pageName) {
-        return match.input
+        return match.input;
       }
 
       if (isAbsolute(pageName)) {
-        pageName = removeInitialSlashes(pageName)
-        href = options.baseURL + pageName + options.uriSuffix
+        pageName = removeInitialSlashes(pageName);
+        href = options.baseURL + pageName + options.uriSuffix;
+      } else {
+        href = options.relativeBaseURL + pageName + options.uriSuffix;
       }
-      else {
-        href = options.relativeBaseURL + pageName + options.uriSuffix
-      }
-      href = utils.escape(href)
+      href = utils.escape(href);
 
-      htmlAttrs.push(`href="${href}"`)
-      for (let attrName in options.htmlAttributes) {
-        const attrValue = options.htmlAttributes[attrName]
-        htmlAttrs.push(`${attrName}="${attrValue}"`)
+      htmlAttrs.push(`href="${href}"`);
+      for (const attrName in options.htmlAttributes) {
+        const attrValue = options.htmlAttributes[attrName];
+        htmlAttrs.push(`${attrName}="${attrValue}"`);
       }
-      htmlAttrsString = htmlAttrs.join(' ')
-      
-      return `<a ${htmlAttrsString}>${label}</a>`
+      htmlAttrsString = htmlAttrs.join(' ');
+
+      return `<a ${htmlAttrsString}>${label}</a>`;
     }
-  )
-}
+  );
+};
